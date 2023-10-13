4c60a91e0ff992ec201f5e730cce8c373b7a37a9
geniustrack <141636583+geniustrack@users.noreply.github.com>
2023-10-13 10:57:59 +0200

    Update Webcomponent_MG.js

--- Webcomponent_MG.js
+++ Webcomponent_MG.js
@@ -44,8 +44,8 @@ tmpl_popup.innerHTML = `
     position: relative;
     top: 0;
     left: 0;
-    width: 50%;
-    height: 50%;
+    width: 100%;
+    height: 100%;
     background-color: rgba(0, 0, 0, 0.5);
     display: flex;
     justify-content: center;
