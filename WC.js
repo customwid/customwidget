87e9464d5917efe60ba53c68620a08c93dde7e73
geniustrack <141636583+geniustrack@users.noreply.github.com>
2024-01-17 14:30:29 +0100

    Update WC.js

--- WC.js
+++ WC.js
@@ -34,6 +34,8 @@
         padding: 20px;
         border-radius: 5px;
         box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+        height: 400px;
+        width: 400px;
        //  display: flex;
       //  flex-direction: column;
         align-items: center;
