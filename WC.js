05d7e67058c9a77c5de6be02280800ec30b4d329
geniustrack <141636583+geniustrack@users.noreply.github.com>
2024-01-22 15:26:41 +0100

    Update WC.js

--- WC.js
+++ WC.js
@@ -15,7 +15,19 @@
         this.dispactchEvent(event);
       });
       const ctx = document.getElementById("myChart");
-
+  new Chart(ctx, {
+        type: "bar",
+        data: {
+          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+          datasets: [
+            {
+              label: "# of Votes",
+              data: [12, 19, 3, 5, 2, 3],
+              borderWidth: 1,
+            },
+          ],
+        },
+      });
       
     }}
   customElements.define("cw-simplified", Visuals);
