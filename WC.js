3e4e355e4586218452ad094e5f11c8bb35f0d89a
geniustrack <141636583+geniustrack@users.noreply.github.com>
2024-01-22 15:22:35 +0100

    Update WC.js

--- WC.js
+++ WC.js
@@ -1,28 +1,10 @@
 (function () {
   let template = document.createElement("template");
-  template.innerHTML = `<style>  
-  #popup {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.5);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  </style>
+  template.innerHTML = `
+     <div class="container" style='display: block'>
   <canvas id="myChart" width="600" height="400"></canvas>
-
-    <script src='https://cdn.jsdelivr.net/npm/chart.js'></script>
-    <script src='https://geniustrack.github.io/customwidget.github.io/chart.js'></script>
-
-     
-     <meta http-equiv="Content-Security-Policy" content="default-src * self blob: data: gap:; style-src * self 'unsafe-inline' blob: data: gap:; script-src * 'self' 'unsafe-eval' 'unsafe-inline' blob: data: gap:; object-src * 'self' blob: data: gap:; img-src * self 'unsafe-inline' blob: data: gap:; connect-src self * 'unsafe-inline' blob: data: gap:; frame-src * self blob: data: gap:;">
-
-     <script>  
-     </script>`;
+        <script src='https://cdn.jsdelivr.net/npm/chart.js'></script>
+    </div>`;
   class Visuals extends HTMLElement {
     constructor() {
       super();
