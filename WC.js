7f0aceae4a8372e0e06a7148056c8c3af0255329
geniustrack <141636583+geniustrack@users.noreply.github.com>
2024-01-22 16:25:33 +0100

    Update WC.js

--- WC.js
+++ WC.js
@@ -1,35 +1,10 @@
 (function () {
   let template = document.createElement("template");
-  template.innerHTML = `<div>
-  <canvas id="myChart"></canvas>
-</div>
-
-<meta http-equiv="Content-Security-Policy" content="default-src 'self' data: gap: https://cdn.jsdelivr.net 'unsafe-eval'; style-src 'self' 'unsafe-inline'; media-src *">
-
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
-<script>
-  const ctx = document.getElementById('myChart');
-
-  new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
-    }
-  });
-</script>`;
+  template.innerHTML = `
+     <div class="container" style='display: block'>
+  <canvas id="myChart" width="600" height="400"></canvas>
+        <script src='https://cdn.jsdelivr.net/npm/chart.js'></script>
+    </div>`;
   class Visuals extends HTMLElement {
     constructor() {
       super();
@@ -53,7 +28,7 @@
   //         ],
   //       },
   //     });
-      
+  
     }}
   customElements.define("cw-simplified", Visuals);
 })();
